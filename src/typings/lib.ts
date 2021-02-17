19773e70bbb1f77a4954771a55ced524649f931f
jpbberry <jb@jt3ch.net>
2021-02-17 17:39:29 -0500

    Document CommandContext

--- src/typings/lib.ts
+++ src/typings/lib.ts
@@ -4,8 +4,17 @@ import worker from '../clustering/worker/Worker'
 namespace DiscordRose {
   export type CommandType = string | RegExp
   export interface CommandOptions {
+    /**
+     * Command to check for (string or RegExp)
+     */
     command: CommandType
+    /**
+     * Array of extra commands to check
+     */
     aliases?: CommandType[]
+    /**
+     * Execute function
+     */
     exec: (ctx: CommandContext) => void | Promise<void>
   }
   export interface CommandContext extends ctx {}
