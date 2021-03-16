44b97d7f66dece8e1936dcb35c7840d1f5c77946
jpbberry <jb@jt3ch.net>
2021-03-15 23:55:26 -0400

    Fix to weird eslint rule causing custom types not to work

--- src/typings/lib.ts
+++ src/typings/lib.ts
@@ -1,20 +1,25 @@
 import { CommandContext as ctx } from '../structures/CommandContext'
 import { Worker as worker } from '../clustering/worker/Worker'
 
-export type CommandType = string | RegExp
-export interface CommandOptions {
-  /**
-   * Command to check for (string or RegExp)
-   */
-  command: CommandType
-  /**
-   * Array of extra commands to check
-   */
-  aliases?: CommandType[]
-  /**
-   * Execute function
-   */
-  exec: (ctx: CommandContext) => void | Promise<void>
+// eslint-disable-next-line @typescript-eslint/no-namespace
+namespace DiscordRose {
+  export type CommandType = string | RegExp
+  export interface CommandOptions {
+    /**
+     * Command to check for (string or RegExp)
+     */
+    command: CommandType
+    /**
+     * Array of extra commands to check
+     */
+    aliases?: CommandType[]
+    /**
+     * Execute function
+     */
+    exec: (ctx: CommandContext) => void | Promise<void>
+  }
+  export interface CommandContext extends ctx {}
+  export type Worker = worker
 }
-export interface CommandContext extends ctx { }
-export type Worker = worker
+
+export = DiscordRose
