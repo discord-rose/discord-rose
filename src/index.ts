7fb43cd580664a2a85136f7e169b31766d7b6ffd
jpbberry <jb@jt3ch.net>
2021-04-06 14:21:38 -0400

    New workflows

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-export { Master } from './clustering/master/Master'
-export * from './clustering/worker/Worker'
-export * from './structures/Embed'
-export * from './clustering/worker/Thread'
-export { PermissionsUtils } from './utils/Permissions'
-export { RestManager } from './rest/Manager'
+export * from './clustering/master/Master'
+export * from './clustering/master/Cluster'
+export * from './clustering/master/Sharder'
 
+export * from './clustering/worker/Worker'
+export * from './clustering/worker/Thread'
+
+export * from './rest/Manager'
+export * from './rest/resources/Channels'
+export * from './rest/resources/Guilds'
+export * from './rest/resources/Members'
+export * from './rest/resources/Messages'
+export * from './rest/resources/Misc'
+export * from './rest/resources/Users'
+export * from './rest/resources/Webhooks'
+
+export * from './socket/Shard'
+
+export * from './structures/Embed'
+export * from './structures/CommandHandler'
 export * from './structures/CommandContext'
+
+export { PermissionsUtils } from './utils/Permissions'
 
 export { CommandOptions } from './typings/lib'
