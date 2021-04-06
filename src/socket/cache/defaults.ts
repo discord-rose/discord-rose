a1d9d6ea26be8968a6e85860ef1d98e8f3a4b55a
jpbberry <ninja.jpb@gmail.com>
2021-04-06 14:25:32 -0400

    Prepare branch

--- src/socket/cache/defaults.ts
+++
@@ -1,8 +0,0 @@
-import { Worker } from '../../clustering/worker/Worker'
-import { CacheManager } from '../CacheManager'
-
-export function defaults (events: CacheManager, worker: Worker): void {
-  events.on('USER_UPDATE', (user) => {
-    if (user.id === worker.user.id) worker.user = user
-  })
-}
