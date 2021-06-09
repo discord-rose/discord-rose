3491fdce075440debdddee26f2e1052c1518c18b
jpbberry <jb@jt3ch.net>
2021-06-09 23:54:43 +0000

    build: tsc build for d0d24c3d400207a2c62996b9168b97055ea06de0

--- dist/socket/cache/voiceStates.js
+++ dist/socket/cache/voiceStates.js
@@ -36,26 +36,37 @@ function voiceStates(events, worker) {
     });
     events.on('VOICE_STATE_UPDATE', (data) => {
         const currentSession = worker.voiceStates.find(x => x.users.some(x => x.session_id === data.session_id));
-        if (!data.guild_id)
+        let voiceState = Object.assign({}, data);
+        if (worker.options.cacheControl.voiceStates) {
+            const newVoiceState = {};
+            worker.options.cacheControl.voiceStates.forEach(key => {
+                newVoiceState[key] = data[key];
+            });
+            newVoiceState.guild_id = voiceState.guild_id;
+            newVoiceState.channel_id = voiceState.channel_id;
+            newVoiceState.user_id = voiceState.user_id;
+            voiceState = newVoiceState;
+        }
+        if (!voiceState.guild_id)
             return;
-        if (!currentSession && data.channel_id) {
-            const channel = getVoiceState(data.channel_id, data.guild_id);
-            channel.users.set(data.user_id, data);
+        if (!currentSession && voiceState.channel_id) {
+            const channel = getVoiceState(voiceState.channel_id, voiceState.guild_id);
+            channel.users.set(voiceState.user_id, voiceState);
         }
-        else if (currentSession && !data.channel_id) {
-            currentSession.users.delete(data.user_id);
+        else if (currentSession && !voiceState.channel_id) {
+            currentSession.users.delete(voiceState.user_id);
             if (currentSession.users.size < 1)
                 worker.voiceStates.delete(currentSession.channel_id);
         }
-        else if (currentSession && data.channel_id && currentSession.channel_id !== data.channel_id) {
-            currentSession === null || currentSession === void 0 ? void 0 : currentSession.users.delete(data.user_id);
+        else if (currentSession && voiceState.channel_id && currentSession.channel_id !== voiceState.channel_id) {
+            currentSession === null || currentSession === void 0 ? void 0 : currentSession.users.delete(voiceState.user_id);
             if (currentSession.users.size < 1)
                 worker.voiceStates.delete(currentSession.channel_id);
-            const channel = getVoiceState(data.channel_id, data.guild_id);
-            channel.users.set(data.user_id, data);
+            const channel = getVoiceState(voiceState.channel_id, voiceState.guild_id);
+            channel.users.set(voiceState.user_id, voiceState);
         }
         else {
-            currentSession === null || currentSession === void 0 ? void 0 : currentSession.users.set(data.user_id, data);
+            currentSession === null || currentSession === void 0 ? void 0 : currentSession.users.set(voiceState.user_id, voiceState);
         }
     });
 }
